36699e33310c388a9ce4c72b6bc209689c4de888
Geeck03 <aramaea.marie@gmail.com>
2025-03-09 17:17:14 -0500

    Care Module Completed

--- Memory App/Model/Card.swift
+++ Memory App/Model/Card.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import CoreGraphics
+
+struct Card: Identifiable, Codable {
+    
+    //Assigns a unique identifer to each instance of the class. Generates a random unique value
+    
+    //ID for each card
+    var id = UUID()
+
+    //There is a default emoji, but it can be changed
+    var emoji: String
+    var isMatched: Bool
+    var isFaceUp: Bool
+    var xPosition: CGFloat
+    var yPosition: CGFloat
+    
+    //This intializes the NoteOBject with this information.
+    init(emoji: String = "💕", isMatched: Bool = false, isFaceUp: Bool = false, xPosition: CGFloat = 0.0, yPosition: CGFloat = 0.0) {
+        self.emoji = emoji
+        self.isMatched = isMatched
+        self.isFaceUp = isFaceUp
+        self.xPosition = xPosition
+        self.yPosition = yPosition
+    }
+}
