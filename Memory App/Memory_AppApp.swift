ccca594514a1dd92ad3eaaccac3eb358368ccbf6
Geeck03 <aramaea.marie@gmail.com>
2025-03-09 16:28:20 -0500

    Initial Commit

---
+++ Memory App/Memory_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_AppApp.swift
+//  Memory App
+//
+//  Created by Sewell, Aramaea on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Memory_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
